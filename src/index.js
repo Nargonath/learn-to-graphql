8a3282eb148dcd7924121cc2535e9fd7b6f65cc5
Jonas Pauthier <jonas.pauthier@gmail.com>
2018-07-22 13:12:54 +0200

    Custom type and resolvers

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
+
+const links = [
+  {
+    id: 'link-0',
+    url: 'www.google.com',
+    description: 'search engine web site',
+  },
+];
 
 const resolvers = {
   Query: {
     info: () => null,
+    feed: () => links,
+  },
+  Link: {
+    id: root => root.id,
+    description: root => root.description,
+    url: root => root.url,
   },
 };
 
