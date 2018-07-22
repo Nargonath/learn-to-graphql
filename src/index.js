dd958255ffd2af7dd8b883fced9b429ace3e37e2
Jonas Pauthier <jonas.pauthier@gmail.com>
2018-07-22 13:59:02 +0200

    Export schema in file and CRUD

--- src/index.js
+++ src/index.js
@@ -1,40 +1,55 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
-const links = [
+let links = [
   {
     id: 'link-0',
     url: 'www.google.com',
     description: 'search engine web site',
   },
 ];
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => null,
     feed: () => links,
+    link: (root, { id }) => links.find(l => l.id === id),
   },
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url,
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links = [...links, link];
+      return link;
+    },
+    updateLink: (root, args) => {
+      const linkIndex = links.findIndex(l => l.id === args.id);
+      if (linkIndex === -1) {
+        return null;
+      }
+
+      const updatedLink = { ...links[linkIndex], ...args };
+      links = [...links.slice(0, linkIndex), updatedLink, ...links.slice(linkIndex + 1)];
+      return updatedLink;
+    },
+    deleteLink: (root, { id }) => {
+      const linkIndex = links.findIndex(l => l.id === id);
+      if (linkIndex === -1) {
+        return null;
+      }
+
+      const deletedLink = links[linkIndex];
+      links = [...links.slice(0, linkIndex), ...links.slice(linkIndex + 1)];
+      return deletedLink;
+    },
   },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 server.start(() => console.log('Server is running on localhost:4000'));
